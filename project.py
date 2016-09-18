3279e3ef48f99224334615e778570c5145dc6215
mandy4994 <mandeep4994@gmail.com>
2016-09-18 20:37:27 +1000

    Added routes for item CRUD

--- project.py
+++ project.py
@@ -28,6 +28,20 @@ def CategoryMenu():
         output += '</br>'
     return output
 
+# Route for new category item function
+
+@app.route('/category/<int:category_id>/new')
+def newCategoryItem(category_id):
+	return "page to create a new item!"
+# Route for edit category item function
+@app.route('/category/<int:category_id>/<int:item_id>/edit')
+def editCategoryItem(category_id, item_id):
+	return "page to edit item!"
+# Route for deleting category item function
+@app.route('/category/<int:category_id>/<int:item_id>/delete')
+def deleteCategoryItem(category_id, item_id):
+	return "page to delete an item!"
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
